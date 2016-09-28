9b2e1fb318dd69d0eac1753b26964e84e9bb66fc
Aile Way <graysvander@gmail.com>
2016-09-28 14:48:23 +0300

    дописать инвертирования, горизонтальное есть на 50%

--- src/main/java/uits/jv1608/sokolov/basicconstructions/triangleArray.java
+++ src/main/java/uits/jv1608/sokolov/basicconstructions/triangleArray.java
@@ -1,20 +1,51 @@
 package uits.jv1608.sokolov.basicconstructions;
 
+import java.util.Random;
+
 
 public class triangleArray {
     public static void main(String[] args) {
-        int[][] ta = new int[10][];
-        for (int i = 0; i < 10; i++) {
-            ta[10] = new int [i+1];
+        trianglearray(args);
+        System.out.println("");
+//        horizontalInverted(triangle_array);
+    }
+        
+    public static void trianglearray (String[] args){
+        Random r = new Random();
+            int m = r.nextInt(20);
+//            System.out.println(m);
+            int[][] triangle_array = new int[m][];
+            for (int i = 0; i < triangle_array.length; i++) {
+                triangle_array[i] = new int [i+1];
+            }
+
+            for (int i = 0; i < triangle_array.length; i++) {
+                int k = 1;
+                for (int j = 0; j < i+1; j++) {
+                    triangle_array[i][j] = k;
+                    k++;
+                    System.out.print(triangle_array[i][j]+" ");
+                }
+                System.out.println("");
+            }
             
-        }
-        public static void arr (ta[][]){
-            
+    
+    }
+    
+    public static void horizontalInverted(int[][] triangle_array) {
+        for (int i = 0; i < triangle_array.length; i++) {
+            for (int j = 0; j < triangle_array.length/2; j++) {
+                int tmp = triangle_array[i][j];
+                triangle_array[i][j] = triangle_array[i][triangle_array.length - j - 1];
+                triangle_array[i][triangle_array.length - j - 1] = tmp;
+                
+                System.out.print(triangle_array[i][j]+" ");
+            }
+            System.out.println("");
         }
     }
     
-    
-    
+ 
     
     
     
